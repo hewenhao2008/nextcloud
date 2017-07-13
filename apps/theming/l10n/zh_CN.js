4a71fb9334bd46ae18bc336b1755389a90e9b903
ZengFei Zhang <zhangzengfei@kunteng.org>
2017-07-13 14:09:35 +0800

    added some translations

---
+++ apps/theming/l10n/zh_CN.js
@@ -0,0 +1,11 @@
+OC.L10N.register(
+    "Theming",
+    {
+    "Theming" : "主题",
+    "Name" : "名称",
+    "Web address" : "网址",
+    "Slogan" : "广告语",
+    "Color" : "颜色",
+    "Log in image" : "登陆背景"
+},
+"nplurals=1; plural=0;");
---
+++ apps/workflowengine/l10n/zh_CN.js
@@ -0,0 +1,6 @@
+OC.L10N.register(
+    "Workflow",
+    {
+    "Workflow" : "工作流"
+},
+"nplurals=1; plural=0;");
